634813e0e436381ce554025ae739fc0dd8f74652
Jujupy <justin.duhez@isen.yncrea.fr>
2020-10-13 17:53:19 +0200

    Adding buttonStylizedContained

---
+++ src/Components/StylizedComponent/TextFieldStylizedOutlined/ButtonStylizedContained/buttonStylizedContained.js
@@ -0,0 +1,16 @@
+import { Button, Grid } from '@material-ui/core'
+import {useStyle} from "./style";
+import React from 'react'
+
+export default function ButtonStylizedContained(props) {
+
+    const classes = useStyle()
+
+    return(
+
+        <Button className={classes.button} variant="contained" color="primary" disableElevation 
+            onClick={props.onClickFunction}>
+            {props.text}
+        </Button>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import App from './App';
 import theme from "./theme";
 import {ThemeProvider} from '@material-ui/core/styles'
 import Header from "./Components/Header/header";
+import ButtonStylizedContained from './Components/StylizedComponent/TextFieldStylizedOutlined/ButtonStylizedContained/buttonStylizedContained';
 
 ReactDOM.render(
   <React.StrictMode>
       <ThemeProvider theme={theme}>
           <Header />
+          <ButtonStylizedContained text="COMMENCER LE TEST" 
+            onClickFunction={() => {
+              console.log("clicked")}
+            }>
+          </ButtonStylizedContained>
       </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
