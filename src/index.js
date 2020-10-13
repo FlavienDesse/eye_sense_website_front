136f00efa9166cd9b371cb5b1174ab65ef727e9f
MrFlavZz <flav621@gmail.com>
2020-10-13 17:00:36 +0200

    finished stylized textField

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import App from './App';
 import theme from "./theme";
 import {ThemeProvider} from '@material-ui/core/styles'
 import Header from "./Components/Header/header";
+import TextFieldStylizedOutlined from "./Components/StylizedComponent/TextFieldStylizedOutlined/textField";
 
 ReactDOM.render(
   <React.StrictMode>
       <ThemeProvider theme={theme}>
-          <Header />
+          <TextFieldStylizedOutlined label={"Mdr"}></TextFieldStylizedOutlined>
       </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
