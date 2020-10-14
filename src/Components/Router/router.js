ec433c233ff21e18eed7ba53c6eedbc8fbcf43d0
MrFlavZz <flav621@gmail.com>
2020-10-14 08:36:04 +0200

    add main route

--- src/Components/Router/router.js
+++ src/Components/Router/router.js
@@ -1,13 +1,26 @@
 import React from "react";
 import {BrowserRouter,Route,Switch} from "react-router-dom";
-
+import MainPage from "../MainPage/mainPage";
 
 export default function Router(){
     return(
         <BrowserRouter>
             <Switch>
-                <Route path={"/"} exact></Route>
-                <Route path={"*"} exact></Route>
+                <Route path={"/"} exact>
+                    <MainPage/>
+                </Route>
+                <Route path={"/CreateCategory"} exact>
+                    <MainPage/>
+                </Route>
+                <Route path={"/AddPhotoToCategory"} exact>
+                    <MainPage/>
+                </Route>
+                <Route path={"/StartTest"} exact>
+                    <MainPage/>
+                </Route>
+                <Route path={"*"} exact>
+                    <MainPage/>
+                </Route>
             </Switch>
         </BrowserRouter>
     )
