1f272db72a11ff6a5e0e3af3ac191f19a1c90fa0
MrFlavZz <flav621@gmail.com>
2020-10-14 09:00:03 +0200

    add history push on button

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import {ThemeProvider} from '@material-ui/core/styles'
 import Header from "./Components/Header/header";
 import ButtonStylizedContained from './Components/StylizedComponent/ButtonStylizedContained/buttonStylizedContained';
 import MainPage from "./Components/MainPage/mainPage";
+import Router from "./Components/Router/router";
 
 ReactDOM.render(
   <React.StrictMode>
       <ThemeProvider theme={theme}>
-            <MainPage></MainPage>
+          <Router/>
       </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
