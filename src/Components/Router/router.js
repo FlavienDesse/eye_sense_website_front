98a88f4fca1a1cf89abc0c73a140e22ea1836569
MrFlavZz <flav621@gmail.com>
2020-10-14 08:55:46 +0200

    optimization

--- src/Components/Router/router.js
+++ src/Components/Router/router.js
@@ -1,26 +1,17 @@
 import React from "react";
-import {BrowserRouter,Route,Switch} from "react-router-dom";
+import {BrowserRouter, Switch} from "react-router-dom";
 import MainPage from "../MainPage/mainPage";
+import PublicRoute from "./publicRoute";
 
-export default function Router(){
-    return(
+export default function Router() {
+    return (
         <BrowserRouter>
             <Switch>
-                <Route path={"/"} exact>
-                    <MainPage/>
-                </Route>
-                <Route path={"/CreateCategory"} exact>
-                    <MainPage/>
-                </Route>
-                <Route path={"/AddPhotoToCategory"} exact>
-                    <MainPage/>
-                </Route>
-                <Route path={"/StartTest"} exact>
-                    <MainPage/>
-                </Route>
-                <Route path={"*"} exact>
-                    <MainPage/>
-                </Route>
+                <PublicRoute path={"/"} exact component={MainPage}/>
+                <PublicRoute path={"/CreateCategory"} exact component={MainPage}/>
+                <PublicRoute path={"/AddPhotoToCategory"} exact component={MainPage}/>
+                <PublicRoute path={"/StartTest"} exact component={MainPage}/>
+                <PublicRoute path={"*"} exact component={MainPage}/>
             </Switch>
         </BrowserRouter>
     )
