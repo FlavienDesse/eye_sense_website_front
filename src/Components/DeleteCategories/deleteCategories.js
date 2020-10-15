a20beab143d1d09a382ca75edf35437089564792
Jujupy <justin.duhez@isen.yncrea.fr>
2020-10-15 12:02:46 +0200

    start DeleteCategories

---
+++ src/Components/DeleteCategories/deleteCategories.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "../Header/header";
+import {Container} from "@material-ui/core";
+import {useStyle} from "./style";
+
+
+export default function CreateCategories() {
+    const classes = useStyle()
+    
+    
+
+    return (
+        <div>
+
+            <Header/>
+            <Container maxWidth="xl">
+                
+
+            </Container>
+        </div>
+
+    )
+}
--- src/Components/Router/router.js
+++ src/Components/Router/router.js
@@ -4,6 +4,7 @@ import MainPage from "../MainPage/mainPage";
 import PublicRoute from "./publicRoute";
 import MenuForm from "../MenuForm/menuForm";
 import CreateCategories from "../CreateCategories/createCategories";
+import DeleteCategories from "../DeleteCategories/deleteCategories"
 
 export default function Router() {
     return (
@@ -11,6 +12,7 @@ export default function Router() {
             <Switch>
 
                 <PublicRoute path={"/CreateCategory"} exact component={CreateCategories}/>
+                <PublicRoute path={"/DeleteCategory"} exact component={DeleteCategories}/>
                 <PublicRoute path={"/AddPhotoToCategory"} exact component={MainPage}/>
                 <PublicRoute path={"/StartTest"} exact component={MenuForm}/>
                 <PublicRoute path={"/"} exact component={MainPage}/>
